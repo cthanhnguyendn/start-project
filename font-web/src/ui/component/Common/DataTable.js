e93a385902b49ba7e3f17ce04572b5be0374c1b7
cthanhnguyen <cthanhnguyen@github.com>
2016-05-26 18:02:06 +0700

    cm

--- font-web/src/ui/component/Common/DataTable.js
+++ font-web/src/ui/component/Common/DataTable.js
@@ -3,6 +3,7 @@
  */
 import React from 'react'
 import BasicTable from 'BasicTable'
+import {FlexTable,AutoSizer,FlexColumn} from 'react-virtualized'
 
 
 
@@ -12,14 +13,53 @@ class DataTable extends BasicTable {
     }
     render(){
         return (
-            <table className="table table-bordered">
-                <thead>
-
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>)
+            <AutoSizer disableHeight>
+                {({ width }) => (
+                    <FlexTable
+                        ref='Table'
+                        headerHeight={50}
+                        height={100}
+                        rowClassName={::this._rowClassName}
+                        rowHeight={useDynamicRowHeight ? this._getRowHeight : rowHeight}
+                        rowGetter={rowGetter}
+                        rowCount={rowCount}
+                        scrollToIndex={scrollToIndex}
+                        sort={this._sort}
+                        sortBy={sortBy}
+                        sortDirection={sortDirection}
+                        width={width}
+                    >
+                        {!hideIndexRow &&
+                        <FlexColumn
+                            label='Index'
+                            cellDataGetter={
+                      ({ columnData, dataKey, rowData }) => rowData.index
+                    }
+                            dataKey='index'
+                            disableSort={!this._isSortEnabled()}
+                            width={60}
+                        />
+                        }
+                        <FlexColumn
+                            dataKey='name'
+                            disableSort={!this._isSortEnabled()}
+                            headerRenderer={this._headerRenderer}
+                            width={90}
+                        />
+                        <FlexColumn
+                            width={210}
+                            disableSort
+                            label='The description label is really long so that it will be truncated'
+                            dataKey='random'
+                            className={styles.exampleColumn}
+                            cellRenderer={
+                    ({ cellData, columnData, dataKey, rowData, rowIndex }) => <div><span>test</span> {cellData}</div>
+                  }
+                            flexGrow={1}
+                        />
+                    </FlexTable>
+                )}
+            </AutoSizer>)
     }
 }
 
